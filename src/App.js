c37bebe99e2a4e6d36ed502c7a70f080c0cfc310
heedrhiss <muyideenidris77@gmail.com>
2024-03-26 11:09:27 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import { Question } from './Quest';
 const initialState = {
   questions: [],
   status: "loading",
-  index: 0
+  index: 0,
+  answer: null,
+  points: 0,
 }
 
 function reducer(state, action){
@@ -26,13 +28,21 @@ function reducer(state, action){
     case 'start':
     return {...state, status: 'start'}
 
+    case 'answer':
+      const curQuestion = state.questions[state.index];
+      return{...state, answer: action.payload,
+      points: curQuestion.correctOption === action.payload ? state.points + curQuestion.points : state.points}
+
+    case 'next':
+      return{...state, index: state.index + 1, answer: null}
+
   default:
       throw new Error("Unknown Action")
   }
 }
 
 function App() {
-const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
+const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
 const num = questions.length;
 
 useEffect(function(){
@@ -55,10 +65,18 @@ async function fetchQuest(){
         {status === "loading" && <Loader />}
         {status === "ready" && <Start num={num} dispatch={dispatch}/>}
         {status === "error" && <Error />}
-        {status === "start" && <Question question={questions[index]} index={index} dispatch={dispatch}/>}
+        {status === "start" && <Question question={questions[index]} index={index} dispatch={dispatch} answer={answer}/>}
+        <Next answer={answer} dispatch={dispatch}/>
       </Main>
     </div>
   );
+}
+
+function Next({dispatch, answer}){
+  if(answer === null ) return;
+  return(
+    <button className='btn btn-ui' onClick={()=>dispatch({type: 'next'})}>Next</button>
+  )
 }
 
 export default App;
